1bc275c270cce3dc3ad1b0b0c4ad9143df13a356
buddyguy423 <abg41493@gmail.com>
2019-04-28 00:00:50 -0400

    first commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article one',
+            'body':'lorem',
+            'author':'Austin Brantley',
+            'created_date':'03-25-2019',
+        },
+        {
+            'id':2,
+            'title':'Article two',
+            'body':'lorem',
+            'author':'Scott Brantley',
+            'created_date':'03-25-2019',
+        },
+        {
+            'id':3,
+            'title':'Article three',
+            'body':'lorem',
+            'author':'Hunter Brantley',
+            'created_date':'03-25-2019',
+        }
+    ]
+    return articles
